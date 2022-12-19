1ee58434bab70c574be0233b13b8e90760eb2210
KatiaAnalista <katiadocita@gmail.com>
2022-12-19 08:24:47 -0300

    Implementando logs na classe PessoaController

--- src/main/java/br/com/zup/edu/personmanager/model/PessoaController.java
+++ src/main/java/br/com/zup/edu/personmanager/model/PessoaController.java
@@ -1,5 +1,7 @@
 package br.com.zup.edu.personmanager.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +13,18 @@ import javax.validation.Valid;
 @RequestMapping("/pessoas")
 public class PessoaController {
 
+    Logger logger = LoggerFactory.getLogger(PessoaController.class);
+
     @Autowired
     private PessoaRepository pessoaRepository;
 
     @PostMapping
     public ResponseEntity<?> inserir(@Valid @RequestBody PessoaRequest request){
+
+
+        logger.info("Inserido com sucesso!");
+        logger.warn("Inserido");
+        logger.error("Falha! Cadastrado não realizado");
 
         var novaPessoa = request.toModel();
 
